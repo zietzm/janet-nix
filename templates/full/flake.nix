f5367842ef320464092abed5f3266f244c4754b7
zietzm <michael.zietz@gmail.com>
2025-06-29 16:22:46 -0700

    drop jfmt and judge

--- templates/full/flake.nix
+++ templates/full/flake.nix
@@ -19,16 +19,15 @@
           inherit system;
           overlays = [ self.overlay ];
         });
-    in {
+    in
+    {
       overlay = final: prev: {
-
-        jpm = prev.jpm.overrideAttrs (old: rec {
+        jpm = prev.jpm.overrideAttrs (old: {
           src = builtins.fetchGit {
             url = "https://github.com/janet-lang/jpm.git";
             rev = "6771439785aea36c76c5aec7c2d7f67df83c46bb";
           };
         });
-
       };
 
       packages = forAllSystems (system: {
@@ -38,42 +37,25 @@
           src = ./.;
           quickbin = ./init.janet;
         };
-        jfmt = janet-nix.packages.${system}.mkJanet {
-          name = "jfmt";
-          src = builtins.fetchGit {
-            url = "https://github.com/andrewchambers/jfmt.git";
-            rev = "b27dff6bb32b89b20462eec33f50c1583c301b0a";
-          };
-          quickbin = "./jfmt.janet";
-        };
       });
 
       defaultPackage =
         forAllSystems (system: self.packages.${system}.my-new-program);
 
       devShell = forAllSystems (system:
-        with nixpkgsFor.${system};
-        let jfmt = self.packages.${system}.jfmt;
-        in mkShell {
-          packages = [ janet jpm jfmt ];
+        with nixpkgsFor.${system}; mkShell {
+          packages = [ janet jpm ];
           buildInputs = [ janet ];
           shellHook = ''
             # localize jpm dependency paths
             export JANET_PATH="$PWD/.jpm"
             export JANET_TREE="$JANET_PATH/jpm_tree"
-            export JANET_LIBPATH="${pkgs.janet}/lib"
-            export JANET_HEADERPATH="${pkgs.janet}/include/janet"
+            export JANET_LIBPATH="$JANET_PATH/lib"
+            export JANET_HEADERPATH="$JANET_PATH/include/janet"
             export JANET_BUILDPATH="$JANET_PATH/build"
             export PATH="$PATH:$JANET_TREE/bin"
             mkdir -p "$JANET_TREE"
             mkdir -p "$JANET_BUILDPATH"
-
-            # install judge as a local script
-            if ! command -v judge &> /dev/null
-            then
-                jpm install "https://github.com/ianthehenry/judge.git"
-                exit
-            fi
           '';
         });
     };
